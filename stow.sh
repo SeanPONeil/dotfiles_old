c896d8cecdda41beec93df2f70f9396225e4319f
Sean O'Neil <oneilse14@gmail.com>
2019-12-07 18:54:16 -0800

    Add stow.sh script

---
+++ stow.sh
@@ -0,0 +1,15 @@
+stow asdf
+stow bin
+stow bspwm
+stow compton
+stow fehbg
+stow git
+stow kitty
+stow nvim
+stow polybar
+stow rofi
+stow sxhkd
+stow wallpaper
+stow xdg
+sudo stow xorg -t /usr/share/X11/xorg.conf.d
+stow zsh
