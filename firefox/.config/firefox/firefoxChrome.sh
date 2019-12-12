93456c4efba6d73e4840ea40142b46f44ad7b904
Sean O'Neil <oneilse14@gmail.com>
2019-12-12 11:17:32 -0800

    Add firefox

---
+++ firefox/.config/firefox/firefoxChrome.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cat ~/.cache/wal/colors.css ~/.config/firefox/firefoxTheme.css > ~/Library/Application\ Support/Firefox/Profiles/1nd7i2es.default-release/chrome/userChrome.css
