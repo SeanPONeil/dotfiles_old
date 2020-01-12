b83fd2f391ac15ce74ecf96125f544311bedb842
Sean O'Neil <oneilse14@gmail.com>
2020-01-12 03:51:36 -0500

    Use bash functions in nordvpn status

--- polybar/.config/polybar/scripts/vpn-nordvpn-status.sh
+++ polybar/.config/polybar/scripts/vpn-nordvpn-status.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-STATUS=$(nordvpn status | grep Status | tr -d ' ' | cut -d ':' -f2)
+status() {
+  echo "$(nordvpn status | grep Status | tr -d ' ' | cut -d ':' -f2)"
+}
 
-if [ "$STATUS" = "Connected" ]; then
-    echo "%{A1:nordvpn d:}$(nordvpn status | grep City | cut -d ':' -f2)%{A}"
+city() {
+  echo "$(nordvpn status | grep City | cut -d ':' -f2)"
+}
+
+if [ "$(status)" = "Connected" ]; then
+    echo "%{A1:nordvpn d:}$(city)%{A}"
 else
     echo "%{A1:nordvpn c:}%{A}"
 fi
