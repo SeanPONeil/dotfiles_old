92276480518aa26aea5e907915c25c40b38cad81
Sean O'Neil <oneilse14@gmail.com>
2020-06-25 11:51:56 -0700

    Update antibody install script

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -e
 
-if command -v brew >/dev/null 2>&1; then
-	brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
-	brew install antibody
-else
-	curl -sL https://git.io/antibody | sudo sh -s -- -b /usr/local/bin
+if ! [ -x "$(command -v antibody)" ]; then
+  if command -v brew >/dev/null 2>&1; then
+	  brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
+	  brew install antibody
+  else
+	  curl -sL https://git.io/antibody | sudo sh -s -- -b /usr/local/bin
+  fi
 fi
+
 antibody bundle <"$DOTFILES/antibody/bundles.txt" > ~/.zsh_plugins
 antibody update
